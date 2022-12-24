b01c1dc05b944be350f98c685b845bd6e67938a1
John Peterson <nxh7@duck.com>
2022-12-24 23:07:30 +0330

    Complete mojtama expansion model

--- lib/models/state_model.dart
+++ lib/models/state_model.dart
@@ -1,7 +1,11 @@
+import 'dart:convert';
 import 'dart:math';
 
 import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
+import 'package:mojtama/models/rule_model.dart';
+import 'package:mojtama/services/user_api_service.dart';
+import 'package:provider/provider.dart';
 
 class CheckboxModel extends ChangeNotifier {
   bool _isOwner = false;
@@ -16,9 +20,38 @@ class CheckboxModel extends ChangeNotifier {
 }
 
 class MojtamaStatusExpansionModel extends ChangeNotifier {
-  List<bool> isOpen = [false, false];
+  List<bool> isOpen = [true, true];
+  bool isLoading = false;
+  String financialStatusText = "{}";
+  Rule mojtamaRule = Rule("", "", "", "", "");
+
+  String? get _financialStatusText {
+    return financialStatusText;
+  }
+
   changeIsOpen(index, shouldOpen) {
     isOpen[index] = shouldOpen;
+    notifyListeners();
+  }
+
+  getFinancialStatus() async {
+    toggleLoading();
+    var response = await UserProvider().getFinancialStatus();
+    toggleLoading();
+    financialStatusText = jsonDecode(response)["data"];
+    notifyListeners();
+  }
+
+  getRules() async {
+    toggleLoading();
+    Rule response = await UserProvider().getMojtamaRules();
+    toggleLoading();
+    mojtamaRule = response;
+    notifyListeners();
+  }
+
+  toggleLoading() {
+    isLoading = !isLoading;
     notifyListeners();
   }
 }
