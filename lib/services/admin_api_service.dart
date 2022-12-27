1e914e7979cff6b92f3f4e61dba38b1888dbe240
John Peterson <nxh7@duck.com>
2022-12-27 18:30:01 +0330

    Add get users function

--- lib/services/admin_api_service.dart
+++ lib/services/admin_api_service.dart
@@ -1,5 +1,8 @@
+import 'dart:convert';
+
 import 'package:hive/hive.dart';
 import 'package:http/http.dart' as http;
+import 'package:mojtama/models/user_model.dart';
 
 class AdminProvider {
   String? host;
@@ -23,6 +26,21 @@ class AdminProvider {
     }
   }
 
+  updatePrice(price) async {
+    var url = Uri.parse("$host/adminpanel/update_price/$price");
+    Map<String, dynamic> payload = {
+      "username": _box.get("username"),
+      "password": _box.get("password"),
+    };
+    http.Response request;
+    request = await http.post(url, body: payload);
+    if (request.statusCode == 200) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
   addOrUpdateMojtamaRule(String rule) async {
     var url = Uri.parse("$host/adminpanel/add_mojtama_rules/");
     Map<String, dynamic> payload = {
@@ -33,8 +51,24 @@ class AdminProvider {
     http.Response request;
     request = await http.post(url, body: payload);
     if (request.statusCode == 200) {
-      print(request.body);
-      return true; //TODO: implement it later
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  getUsers() async {
+    var url = Uri.parse("$host/adminpanel/users");
+    http.Response request;
+    request = await http.get(url);
+    List<User> users = [];
+    if (request.statusCode == 200) {
+      List listUsers = jsonDecode(request.body);
+      listUsers.forEach((arrUser) {
+        User user = User.fromJson(arrUser);
+        users.add(user);
+      });
+      return users;
     } else {
       return false;
     }
