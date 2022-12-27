e2a3355960f2610217cbe85da5eea5993dd4764c
John Peterson <nxh7@duck.com>
2022-12-27 19:11:47 +0330

    Manage user's payment status state

--- lib/models/payment_model.dart
+++ lib/models/payment_model.dart
@@ -1,11 +1,23 @@
 import 'dart:math';
 
 import 'package:flutter/material.dart';
+import 'package:mojtama/services/user_api_service.dart';
 
 class PaymentModel extends ChangeNotifier {
   String? _text = "ربیع";
   String get text => _text!;
   List testMonths = ["ربیع", "تست", "علی"];
+  String paymentStat = "";
+  getPaymentStatusOfTheUsersInTheMonth() async {
+    UserProvider userProvider = UserProvider();
+    String? ans = await userProvider.getPaymentStatusMessageOfTheUsers();
+    print(ans);
+    if (ans != null) {
+      paymentStat = ans;
+      notifyListeners();
+    }
+  }
+
   randomText() {
     Random rand = Random();
     int randomIndex = rand.nextInt(testMonths.length);
