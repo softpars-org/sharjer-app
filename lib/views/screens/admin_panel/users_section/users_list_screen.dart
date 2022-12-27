7e5a6a91283d5eb93486ff96f7a3e4dbe4c80493
John Peterson <nxh7@duck.com>
2022-12-27 18:39:01 +0330

    Prepare get users

--- lib/views/screens/admin_panel/users_section/users_list_screen.dart
+++ lib/views/screens/admin_panel/users_section/users_list_screen.dart
@@ -1,11 +1,29 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:mojtama/models/adminpanel_model.dart';
 import 'package:mojtama/models/month_model.dart';
+import 'package:mojtama/models/user_model.dart';
 import 'package:mojtama/services/app_service.dart';
 import 'package:provider/provider.dart';
 
-class UsersListScreen extends StatelessWidget {
+class UsersListScreen extends StatefulWidget {
   const UsersListScreen({super.key});
+
+  @override
+  State<UsersListScreen> createState() => _UsersListScreenState();
+}
+
+class _UsersListScreenState extends State<UsersListScreen> {
+  @override
+  void didChangeDependencies() {
+    super.didChangeDependencies();
+    Future.delayed(Duration.zero, () => _loadResources());
+  }
+
+  _loadResources() async {
+    var provider = Provider.of<AdminPanelModel>(context, listen: false);
+    provider.getUsers();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -30,18 +48,33 @@ class UsersListScreen extends StatelessWidget {
             ],
           ),
         ),
-        body: TabBarView(
-          children: [
-            ListView.builder(
-              itemBuilder: (context, index) => UserCard(),
-            ),
-            ListView.builder(
-              itemBuilder: (context, index) => UserCard(),
-            ),
-            ListView.builder(
-              itemBuilder: (context, index) => UserCard(),
-            ),
-          ],
+        body: Consumer<AdminPanelModel>(
+          builder: (context, model, child) {
+            return model.isLoading
+                ? Center(child: CircularProgressIndicator())
+                : TabBarView(
+                    children: [
+                      ListView.builder(
+                        itemCount: model.getUsersLengthOfBluck(1),
+                        itemBuilder: (context, index) => UserCard(
+                          user: model.users[index],
+                        ),
+                      ),
+                      ListView.builder(
+                        itemCount: model.getUsersLengthOfBluck(2),
+                        itemBuilder: (context, index) => UserCard(
+                          user: model.users[index],
+                        ),
+                      ),
+                      ListView.builder(
+                        itemCount: model.getUsersLengthOfBluck(3),
+                        itemBuilder: (context, index) => UserCard(
+                          user: model.users[index],
+                        ),
+                      ),
+                    ],
+                  );
+          },
         ),
       ),
     );
@@ -49,33 +82,47 @@ class UsersListScreen extends StatelessWidget {
 }
 
 class UserCard extends StatelessWidget {
-  const UserCard({super.key});
+  User user;
+  UserCard({super.key, required this.user});
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      padding: EdgeInsets.all(20),
-      child: Card(
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(10),
-        ),
-        child: Padding(
-          padding: const EdgeInsets.all(10.0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              ThreeDots(),
-              Text("نام کاربری: username"),
-              Text("نام و نام خانوادگی: namefamily"),
-              Text("بلوک: ۱"),
-              Text("واحد: ۱۳"),
-              SelectableText("شمارهٔ تلفن: phonenumber"),
-              Text("پلاک خودرو: plak khodro"),
-            ],
-          ),
-        ),
+      child: ExpansionTile(
+        expandedAlignment: Alignment.topRight,
+        childrenPadding: EdgeInsets.all(30),
+        expandedCrossAxisAlignment: CrossAxisAlignment.start,
+        title: Text("${user.name} ${user.family}"),
+        subtitle: Text("بلوک ${user.bluck}، واحد ${user.vahed}"),
+        children: [
+          // Card(
+          //   shape: RoundedRectangleBorder(
+          //     borderRadius: BorderRadius.circular(10),
+          //   ),
+          //   child: Padding(
+          //     padding: const EdgeInsets.all(10.0),
+          //     child: Column(
+          //       crossAxisAlignment: CrossAxisAlignment.start,
+          //       children: [],
+          //     ),
+          //   ),
+          // ),
+          ThreeDots(),
+          Text("نام کاربری: ${user.username}"),
+          Text("نام و نام خانوادگی: ${user.family}"),
+          Text("بلوک: ${user.bluck}"),
+          Text("واحد: ${user.vahed}"),
+          SelectableText("شمارهٔ همراه: ${user.phone}"),
+          SelectableText("شمارهٔ همراه۲: ${user.phone2}"),
+          Text("پلاک خودرو: ${user.carPlate}"),
+        ],
       ),
     );
+
+    // return Container(
+    //   padding: EdgeInsets.all(20),
+    //   child:
+    // );
   }
 }
 
