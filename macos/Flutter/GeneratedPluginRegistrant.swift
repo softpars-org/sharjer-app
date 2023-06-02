ce3edab05848e3142d111bb62fb5b328344c2e03
John Peterson <nxh7@duck.com>
2023-06-02 11:58:40 +0330

    Upgrade to flutter V3.10

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -8,7 +8,7 @@ import Foundation
 import firebase_core
 import firebase_messaging
 import package_info_plus
-import path_provider_macos
+import path_provider_foundation
 import url_launcher_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
